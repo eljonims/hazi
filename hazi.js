3f00c5e1a5fd4db02e78fe0e95d1ced482692c4b
Jonathan Marin <schanejin@gmail.com>
2026-02-21 04:15:05 +0100

    refactor: centralizado registro de sw y carga asíncrona en hazi.js

--- hazi.js
+++ hazi.js
@@ -1,5 +1,20 @@
 class Hazi{
         constructor(){
-                
+
         }
+        lanzar(){}
 }
+
+// --- REGISTRO DEL VIGILANTE (PWA) ---
+
+if ('serviceWorker' in navigator) {
+    // Usamos el evento 'load' de la ventana para no interferir con la carga crítica
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('./sw.js')
+            .then(reg => console.log("Service Worker HAZI activo en el scope:", reg.scope))
+            .catch(err => console.error("Error al registrar Service Worker:", err));
+    });
+}
+
+window.appHazi = new Hazi();
+window.appHazi.lanzar();
